14e120c0df84f8700d5af1dc3845e9dcd4dee21c
Corné <lightscape2001@gmail.com>
2022-07-18 15:00:19 +0200

    fix admin

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ use \Tsugi\Core\Settings;
 
 $LAUNCH = LTIX::requireData();
 
-$is_admin = $LAUNCH->ltiRawParameter('custom_admin','true');
-custom_admin=true
+$is_admin = $LAUNCH->ltiRawParameter('custom_admin', false);
+// custom_admin=true
 
 $menu = false; // We are not using a menu
 if ( $USER->instructor ) {
-    if $is_admin) {
+    if ($is_admin === true) {
         header( 'Location: '.addSession('admin-home.php') ) ;
     } else {
         header( 'Location: '.addSession('instructor-home.php') ) ;
